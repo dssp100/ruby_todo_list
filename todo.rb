f61cf952af3dfa7078940cb3b9e4b3cb8522fa59
Daniel Springer <dssp100@gmail.com>
2018-05-20 16:26:58 -0300

    Added module Menu, Show

--- todo.rb
+++ todo.rb
@@ -1,5 +1,4 @@
 class List
-
   attr_reader :all_tasks
 
   def initialize
@@ -10,20 +9,28 @@ class List
     @all_tasks.push(new_task)
   end
 
-end
+  def show
+    @all_tasks
+  end
+
+end #end of the List class
 
 class Task
   attr_reader :description
 
   def initialize(description)
     @description = description
-
   end
-end
+
+end #end of the Task class
 
 if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
-  puts 'You have created a new list'
-  my_list.add_task(Task.new("Go shopping"))
-  puts "You have created a new task"
-end
+          my_list = List.new
+          puts 'You have created a new list'
+          my_list.add_task(Task.new('Make Breakfast'))
+          my_list.add_task(Task.new('Wash Breakfast Dishes'))
+          my_list.add_task('Send Birthday Gift to Mom')
+          puts 'You have added a task to the Todo List'
+          puts 'Your task list:'
+          puts my_list.show
+        end
