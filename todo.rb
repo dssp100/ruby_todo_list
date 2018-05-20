2557c58db370b388e028145eead713173943fc81
Daniel Springer <dssp100@gmail.com>
2018-05-20 16:28:39 -0300

    Added module Menu, Show

--- todo.rb
+++ todo.rb
@@ -1,3 +1,26 @@
+module Menu
+
+  def menu
+      " Welcome to the TodoList Program!
+      This menu will help you use the Task List System
+      1) Add
+      2) Show
+      Q) Quit "
+    end
+
+    def show_menu
+      menu
+    end
+
+module Promptabe
+
+    def promt(message = "What would you like to do?", symbol = ":> ")
+      print message
+      answer = gets.chomp
+    end
+
+end
+
 class List
   attr_reader :all_tasks
 
@@ -25,12 +48,12 @@ class Task
 end #end of the Task class
 
 if __FILE__ == $PROGRAM_NAME
-          my_list = List.new
-          puts 'You have created a new list'
-          my_list.add_task(Task.new('Make Breakfast'))
-          my_list.add_task(Task.new('Wash Breakfast Dishes'))
-          my_list.add_task('Send Birthday Gift to Mom')
-          puts 'You have added a task to the Todo List'
-          puts 'Your task list:'
-          puts my_list.show
-        end
+  my_list = List.new
+  puts 'You have created a new list'
+  my_list.add_task(Task.new('Make Breakfast'))
+  my_list.add_task(Task.new('Wash Breakfast Dishes'))
+  my_list.add_task('Send Birthday Gift to Mom')
+  puts 'You have added a task to the Todo List'
+  puts 'Your task list:'
+  puts my_list.show
+end
